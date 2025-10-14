24750ffc8ffbbdcc5295e8b3462db6669ee096a0
MCT-Salman <mctmohammad948@gmail.com>
2025-10-14 17:52:19 +0300

    cors img

--- src/utils/corsConfig.js
+++ src/utils/corsConfig.js
@@ -6,7 +6,8 @@
 export const corsConfig = {
   credentials: true,
   headers: {
-    'Access-Control-Allow-Origin': 'https://dev.tallaam.com',
+    // 'Access-Control-Allow-Origin': 'https://dev.tallaam.com',
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     'Access-Control-Allow-Credentials': 'true'
