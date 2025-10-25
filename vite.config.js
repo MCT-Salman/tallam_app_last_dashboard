4c59004bd69c5287d6d4e8a8a41d1852eae4447c
MCT-Salman <mctmohammad948@gmail.com>
2025-10-25 16:52:56 +0300

    fix home page

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  resolve: {
+  plugins: [react(),tailwindcss(),],
+    resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
